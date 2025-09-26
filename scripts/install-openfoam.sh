0345d42525882a7b010e162edf8f6428f03ed3d6
Andrew Davis <andrew.davis@ukaea.uk>
2025-09-26 21:31:08 +0100

    Update to OpenFOAM-13

--- scripts/install-openfoam.sh
+++ scripts/install-openfoam.sh
@@ -9,7 +9,7 @@ BUILD_JOBS=""
 OUT_DIR="$(dirname "${SCRIPT_DIR}")/external/openfoam"
 USAGE="usage: install-openfoam.sh [-h] [-s] [-o DIRECTORY]
 
-Install OpenFOAM-12 for Ubuntu, applying the patch required by hippo.
+Install OpenFOAM-13 for Ubuntu, applying the patch required by hippo.
 
 Note that you will need to install OpenFOAM's requirements separately.
 You can do this by running:
@@ -58,15 +58,15 @@ if [ "$BUILD_JOBS" -eq "0" ]; then
     BUILD_JOBS=""
 fi;
 
-OPENFOAM_DIR="${OUT_DIR}/OpenFOAM-12"
-OPENFOAM_REV="9ec94dd57a8d98c3f3422ce9b2156a8b268bbda6"
-THIRDPARTY_DIR="${OUT_DIR}/ThirdParty-12"
-THIRDPARTY_REV="cab725f5e7929e8f5ec35c54edc493a822355235"
+OPENFOAM_DIR="${OUT_DIR}/OpenFOAM-13"
+OPENFOAM_REV="d5aebccff79d38838956e1353f198ca89139e666"
+THIRDPARTY_DIR="${OUT_DIR}/ThirdParty-13"
+THIRDPARTY_REV="3d868855b80c5d7de18f1f3793e8e5fe94bc168f"
 
 # Fetch and patch OpenFOAM
 mkdir -p "${OPENFOAM_DIR}"
 if [ ! -d "${OPENFOAM_DIR}/.git" ]; then
-    git clone https://github.com/OpenFOAM/OpenFOAM-12.git "${OPENFOAM_DIR}"
+    git clone https://github.com/OpenFOAM/OpenFOAM-13.git "${OPENFOAM_DIR}"
 fi
 git -C "${OPENFOAM_DIR}" reset --hard "${OPENFOAM_REV}"
 git -C "${OPENFOAM_DIR}" apply "${SCRIPT_DIR}/openfoam.patch"
@@ -74,7 +74,7 @@ git -C "${OPENFOAM_DIR}" apply "${SCRIPT_DIR}/openfoam.patch"
 # Set up OpenFOAM
 source "${OPENFOAM_DIR}/etc/bashrc" || true
 
-echo "Hippo installing OpenFOAM-12 with options:"
+echo "Hippo installing OpenFOAM-13 with options:"
 echo "------------------------------------------"
 echo "  WM_ARCH_OPTION:      ${WM_ARCH_OPTION}"
 echo "  WM_COMPILE_OPTION:   ${WM_COMPILE_OPTION}"
@@ -86,7 +86,7 @@ echo "  WM_PRECISION_OPTION: ${WM_PRECISION_OPTION}"
 
 mkdir -p "${THIRDPARTY_DIR}"
 if [ ! -d "${THIRDPARTY_DIR}/.git" ]; then
-    git clone https://github.com/OpenFOAM/ThirdParty-12.git "${THIRDPARTY_DIR}"
+    git clone https://github.com/OpenFOAM/ThirdParty-13.git "${THIRDPARTY_DIR}"
 fi
 git -C "${THIRDPARTY_DIR}" reset --hard "${THIRDPARTY_REV}"
 (
