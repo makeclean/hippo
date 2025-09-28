773475a1ab762597f5fee7b4d1007f7feeeb785d
Andrew Davis <andrew.davis@ukaea.uk>
2025-09-28 20:50:59 +0100

    another go

--- scripts/install-openfoam.sh
+++ scripts/install-openfoam.sh
@@ -91,8 +91,7 @@ fi
 git -C "${THIRDPARTY_DIR}" reset --hard "${THIRDPARTY_REV}"
 (
     cd "${THIRDPARTY_DIR}" \
-    && export ZOLTAN_TYPE=none \
-    && ./Allwmake
+    && export ZOLTAN_TYPE=none ./Allwmake
 )
 
 # Refresh OpenFOAM paths
